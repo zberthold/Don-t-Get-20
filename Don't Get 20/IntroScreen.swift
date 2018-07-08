cc2b62759c46180b5a1084be5d607da22459aecd
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-08 14:13:04 -0500

    Added changeable background color, up and down arrows for the number you have to not get

--- Don't Get 20/IntroScreen.swift
+++ Don't Get 20/IntroScreen.swift
@@ -18,6 +18,7 @@ class IntroScreen: UIViewController {
     @IBOutlet weak var color4: UIButton!
     @IBOutlet weak var dontReachNumber: UILabel!
     var there = true
+    var props = Properties()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +36,45 @@ class IntroScreen: UIViewController {
             there = false
         }
     }
+    func colorClicked (x: Int, color: UIButton){
+        let colors = [color1, color2, color3, color4]
+        for button in colors {
+            button!.layer.borderColor = UIColor.clear.cgColor
+        }
+        self.view.backgroundColor = color.backgroundColor
+        color.frame = CGRect(x:Int((self.view.frame.size.width)/2) - x, y: 230, width: 30, height: 30)
+        color.layer.borderWidth = 2.0
+        color.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    @IBAction func colorOneClicked(_ sender: Any) {
+        colorClicked(x: 135, color: color1)
+    }
+    
+    @IBAction func colorTwoClicked(_ sender: Any) {
+        colorClicked(x: 55, color: color2)
+    }
+    
+    @IBAction func colorThreeClicked(_ sender: Any) {
+        colorClicked(x: -25, color: color3)
+    }
+    
+    @IBAction func colorFourClicked(_ sender: Any) {
+        colorClicked(x: -105, color: color4)
+    }
     
     @IBAction func increaseDontReach(_ sender: Any) {
+        var number = Int(dontReachNumber.text!)!
+        number += 1
+        dontReachNumber.text = String(number)
     }
     
     @IBAction func decreaseDontReach(_ sender: Any) {
+        if Int(dontReachNumber.text!)! > 1 {
+            var number = Int(dontReachNumber.text!)!
+            number -= 1
+            dontReachNumber.text = String(number)
+        }
     }
     
     @IBAction func startGame(_ sender: Any) {
@@ -50,6 +85,12 @@ class IntroScreen: UIViewController {
             performSegue(withIdentifier: "segueofrhea", sender: nil)
         }
     }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let dvc = segue.destination as! ViewController
+//
+//
+//    }
     
 
 }
---
+++ Don't Get 20/Properties.swift
@@ -0,0 +1,27 @@
+//
+//  Properties.swift
+//  Don't Get 20
+//
+//  Created by Zoe Berthold on 7/8/18.
+//  Copyright © 2018 Zoe Berthold. All rights reserved.
+//
+
+import UIKit
+
+class Properties: UIViewController {
+    
+    var backgroundcolor = UIColor.red
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
