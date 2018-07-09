ed6a5ba9e1f6b8aa237d2d45cc68c7f8497b1072
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-09 07:36:50 -0500

    Lets you know when you've won, other basic functions such as back button

--- Don't Get 20/ViewController.swift
+++ Don't Get 20/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var props = Properties()
     var counter = 0
     var numberNow = 0
+    var winner = ""
     @IBOutlet weak var whichPlayer: UILabel!
     @IBOutlet weak var currentNumber: UILabel!
     @IBOutlet weak var dontGetNumber: UILabel!
@@ -23,24 +24,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = props.backgroundcolor
         dontGetNumber.text = "Don't Get \(props.dontReach)!"
+        whichPlayer.text = "\(props.name1)'s turn"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        whichPlayer.text = props.name1
+        
     }
     
     func switchTurn(){
         counter += 1
         let nameArray = [props.name1, props.name2]
         var forCounter = counter % 2
-        whichPlayer.text = "\(nameArray[forCounter])"
+        whichPlayer.text = "\(nameArray[forCounter])'s turn"
         numberNow = Int(currentNumber.text!)!
+    }
+    
+    func whoWon(){
+        if counter % 2 == 0{
+            winner = props.name1
+        }
+        else {
+            winner = props.name2
+        }
     }
     
     func checkForEnd(){
         if numberNow >= props.dontReach{
-            print("game over")
+            whoWon()
+            let alert = UIAlertController(title: "\(winner) won!", message: nil, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Play Again", style: .default) { (action) in
+                self.currentNumber.text = "0"
+                self.counter = 0
+                self.whichPlayer.text = "\(self.props.name1)'s turn"
+                self.numberNow = 0
+            }
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
         }
     }
     
@@ -49,6 +69,7 @@ class ViewController: UIViewController {
         currentNumber.text = String(numberNow)
         switchTurn()
         checkForEnd()
+        
     }
     
     @IBAction func addOne(_ sender: Any) {
@@ -56,9 +77,14 @@ class ViewController: UIViewController {
         currentNumber.text = String(numberNow)
         switchTurn()
         checkForEnd()
+        
     }
     
     @IBAction func reset(_ sender: Any) {
+        self.currentNumber.text = "0"
+        self.counter = 0
+        self.whichPlayer.text = "\(self.props.name1)'s turn"
+        self.numberNow = 0
     }
     
     
